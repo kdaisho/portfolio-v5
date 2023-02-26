e4a9dcb9819f65708de90a0d3f69545d4d7cfdb0
Daisho Komiyama <daishokomiyama@gmail.com>
2023-02-26 08:55:39 -0500

    Set module import aliases for components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import Header from "../components/header/Header.svelte";
-	import Work from "../components/work/Work.svelte";
-	import Hero from "../components/hero/Hero.svelte";
-	import Tooling from "../components/tooling/Tooling.svelte";
-	import Projects from "../components/projects/Projects.svelte";
+	import Header from "$components/header/Header.svelte";
+	import Work from "$components/work/Work.svelte";
+	import Hero from "$components/hero/Hero.svelte";
+	import Tooling from "$components/tooling/Tooling.svelte";
+	import Projects from "$components/projects/Projects.svelte";
 	import type { Filters } from "$lib/types";
 
 	const handleFilterChange = ({ checked, value, filterTerms }: Filters) => {
