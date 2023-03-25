6e68ede0b99863f96e2d11d954b256e14721c12c
Daisho Komiyama <daishokomiyama@gmail.com>
2023-03-25 08:22:44 -0400

    Fix broken hover state for each project

--- src/components/projects/Projects.svelte
+++ src/components/projects/Projects.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 	import { filterItems, projects } from "./data"
+	import type { Project } from "$lib/types"
+
+	let selectedProject: Project | null
+	let activeCardId: number | null
+
+	const handleDisplayProject = (project: Project) => {
+		activeCardId = project.id
+		selectedProject = project
+	}
 </script>
 
 <div class="content-wrap is-side-by-side">
@@ -11,7 +20,7 @@
 				helps me stay up to date.
 			</p>
 		</div>
-		<fieldset class="filter-section ">
+		<fieldset class="filter-section">
 			<button class="toggle-filter outline-button">Filters</button>
 			<legend>(AND) Filters</legend>
 			<div class="filters">
@@ -26,21 +35,37 @@
 		</fieldset>
 	</div>
 	<div class="cards left-side">
-		{#each projects as { title, subtitle, description, stack, hero, thumb, url, githubUrl }}
-			<div class="card" role="button" tabindex="0">
-				<div class="top" style="background: #444 url({thumb}) center center / cover no-repeat">
+		{#each projects as project}
+			<div
+				class="card"
+				class:active={activeCardId === project.id}
+				on:click={() => handleDisplayProject(project)}
+				on:keypress
+				role="button"
+				tabindex="0"
+			>
+				<div
+					class="top"
+					style="background: #444 url({project.thumb}) center center / cover no-repeat"
+				>
 					<div class="glass" />
 				</div>
 				<div class="bottom">
 					<div class="text-group">
-						<h2 class="title">{title}</h2>
+						<h2 class="title">{project.title}</h2>
 						<span class="separator" />
-						<p class="desc">{subtitle}</p>
+						<p class="desc">{project.subtitle}</p>
 					</div>
 				</div>
 			</div>
 		{/each}
 	</div>
+
+	{#if selectedProject}
+		<div>
+			Modal {selectedProject.title} !
+		</div>
+	{/if}
 </div>
 
 <style src="./styles.css"></style>
