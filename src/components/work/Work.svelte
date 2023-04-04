a05b0cbaaca8a7cdefdfaafc38daa0349ecd1939
Daisho Komiyama <daishokomiyama@gmail.com>
2023-04-04 19:29:40 -0400

    Remove hardcoded cards

--- src/components/work/Work.svelte
+++ src/components/work/Work.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { cards } from "./data"
+</script>
+
 <section id="work" class="section is-education bright">
 	<div class="content-wrap is-side-by-side">
 		<div class="title-group">
@@ -8,163 +12,37 @@
 			</p>
 		</div>
 		<div class="cards">
-			<div class="card">
-				<div class="side is-front">
-					<h2 class="company">Fabric.inc</h2>
-					<div class="summary">
-						<h3 class="position">Software Development Engineer</h3>
-						<span class="separator" />
-						<p>Aug 2020 - Sep 2022</p>
-						<p>Remote</p>
+			{#each cards as card}
+				<div class="card">
+					<div class="side is-front">
+						<h2 class="company">{card.name}</h2>
+						<div class="summary">
+							<h3 class="position">{card.title}</h3>
+							<span class="separator" />
+							<p>{card.period}</p>
+							<p>{card.location}</p>
+						</div>
+					</div>
+					<div class="side is-back">
+						<div class="list">
+							<p>Do</p>
+							<ul>
+								{#each card.tasks as task}
+									<li>{task}</li>
+								{/each}
+							</ul>
+						</div>
+						<div class="list">
+							<p>With</p>
+							<ul>
+								{#each card.techStack as tech}
+									<li>{tech}</li>
+								{/each}
+							</ul>
+						</div>
 					</div>
 				</div>
-				<div class="side is-back">
-					<div class="list">
-						<p>Do</p>
-						<ul><li>Headless CMS Development</li></ul>
-					</div>
-					<div class="list">
-						<p>With</p>
-						<ul>
-							<li>React</li>
-							<li>TypeScript</li>
-							<li>Nextjs</li>
-							<li>Nodejs</li>
-							<li>Jest</li>
-							<li>GraphQL</li>
-							<li>Git</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="card">
-				<div class="side is-front">
-					<h2 class="company">Bell Canada</h2>
-					<div class="summary">
-						<h3 class="position">Web Application Developer</h3>
-						<span class="separator" />
-						<p>July 2018 - June 2020</p>
-						<p>Montreal</p>
-					</div>
-				</div>
-				<div class="side is-back">
-					<div class="list">
-						<p>Do</p>
-						<ul><li>Lots of Web Apps</li></ul>
-					</div>
-					<div class="list">
-						<p>With</p>
-						<ul>
-							<li>React</li>
-							<li>AngularJS</li>
-							<li>Javascript</li>
-							<li>HTML5</li>
-							<li>CSS3</li>
-							<li>Less</li>
-							<li>SVN</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="card">
-				<div class="side is-front">
-					<h2 class="company">CGI</h2>
-					<div class="summary">
-						<h3 class="position">Front-end Developer</h3>
-						<span class="separator" />
-						<p>Apr 2018 - June 2018</p>
-						<p>Montreal</p>
-					</div>
-				</div>
-				<div class="side is-back">
-					<div class="list">
-						<p>Do</p>
-						<ul>
-							<li>Enhancement</li>
-							<li>Bug fix</li>
-							<li>HTML Email</li>
-						</ul>
-					</div>
-					<div class="list">
-						<p>With</p>
-						<ul>
-							<li>AngularJS</li>
-							<li>JavaScript</li>
-							<li>HTML5</li>
-							<li>CSS3</li>
-							<li>Less</li>
-							<li>Git</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="card">
-				<div class="side is-front">
-					<h2 class="company">Reitmans</h2>
-					<div class="summary">
-						<h3 class="position">Front-end Integrator</h3>
-						<span class="separator" />
-						<p>Feb 2016 - Mar 2018</p>
-						<p>Montreal</p>
-					</div>
-				</div>
-				<div class="side is-back">
-					<div class="list">
-						<p>Do</p>
-						<ul>
-							<li>Markup</li>
-							<li>Style</li>
-							<li>HTML Email</li>
-							<li>DOM Manipulation</li>
-						</ul>
-					</div>
-					<div class="list">
-						<p>With</p>
-						<ul>
-							<li>HTML5</li>
-							<li>CSS3</li>
-							<li>Sass</li>
-							<li>Susy</li>
-							<li>JavaScript</li>
-							<li>SVN</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="card">
-				<div class="side is-front">
-					<h2 class="company">JF and Sons</h2>
-					<div class="summary">
-						<h3 class="position">Designer/Integrator</h3>
-						<span class="separator" />
-						<p>Jan 2015 - Dec 2015</p>
-						<p>Montreal</p>
-					</div>
-				</div>
-				<div class="side is-back">
-					<div class="list">
-						<p>Do</p>
-						<ul>
-							<li>Design</li>
-							<li>Markup</li>
-							<li>HTML Email</li>
-						</ul>
-					</div>
-					<div class="list">
-						<p>With</p>
-						<ul>
-							<li>HTML5</li>
-							<li>CSS3</li>
-							<li>Sass</li>
-							<li>JavaScript</li>
-							<li>PHP</li>
-							<li>Photoshop</li>
-							<li>Illustrator</li>
-							<li>Git</li>
-						</ul>
-					</div>
-				</div>
-			</div>
+			{/each}
 		</div>
 	</div>
 </section>
---
+++ src/components/work/data.ts
@@ -0,0 +1,47 @@
+export const cards = [
+	{
+		id: "fabric",
+		name: "Fabric.inc",
+		period: "Aug 2020  - Sep 2022",
+		title: "Software Development Engineer",
+		location: "Remote",
+		tasks: ["Headless CMS Development"],
+		techStack: ["React", "TypeScript", "Nextjs", "Nodejs", "Jest", "GraphQL", "Git"]
+	},
+	{
+		id: "bell",
+		name: "Bell Canada",
+		period: "July 2018 - June 2020",
+		title: "Web Application Developer",
+		location: "Montreal",
+		tasks: ["Lots of Web Apps"],
+		techStack: ["React", "AngularJS", "Javascript", "HTML5", "CSS3", "Less", "SVN"]
+	},
+	{
+		id: "cgi",
+		name: "CGI",
+		period: "Apr 2018 - June 2018",
+		title: "Front-end Developer",
+		location: "Montreal",
+		tasks: ["Enhancement", "Bug fix", "HTML Email"],
+		techStack: ["AngularJS", "JavaScript", "HTML5", "CSS3", "Less", "Git"]
+	},
+	{
+		id: "reitmans",
+		name: "Reitmans",
+		period: "Feb 2016 - Mar 2018",
+		title: "Front-end Integrator",
+		location: "Montreal",
+		tasks: ["Markup", "Style", "HTML Email", "DOM Manipulation"],
+		techStack: ["HTML5", "CSS3", "Sass", "Susy", "JavaScript", "SVN"]
+	},
+	{
+		id: "jfandsons",
+		name: "JF and Sons",
+		period: "Jan 2015  - Dec 2015",
+		title: "Designer/Integrator",
+		location: "Montreal",
+		tasks: ["Design", "Markup", "HTML Email"],
+		techStack: ["HTML5", "CSS3", "Sass", "JavaScript", "PHP", "Photoshop", "Illustrator", "Git"]
+	}
+]
