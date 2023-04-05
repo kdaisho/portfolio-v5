71db81f89a5d282d39cf1704dab485e227c615da
Daisho Komiyama <daishokomiyama@gmail.com>
2023-04-04 20:59:44 -0400

    Remove the last scss

--- src/components/tooling/Tooling.svelte
+++ src/components/tooling/Tooling.svelte
@@ -34,7 +34,7 @@
 
 <section id="tooling" class="section is-tooling bright">
 	<div class="content-wrap is-side-by-side">
-		<div class="left-side">
+		<div class="left">
 			<div class="title-group has-filters">
 				<h2 class="title">Tooling</h2>
 				<p class="subtitle font-large">
@@ -61,7 +61,7 @@
 				</div>
 			</fieldset>
 		</div>
-		<div class="tooling right-side">
+		<div class="tooling right">
 			{#each _devIcons as icon (icon.name)}
 				<div
 					class="tool"
@@ -81,105 +81,4 @@
 	</div>
 </section>
 
-<style lang="scss">
-	.is-tooling {
-		.left-side {
-			flex: 0 0 100%;
-
-			@media (min-width: 769px) {
-				flex: 0 0 35%;
-			}
-		}
-
-		svg {
-			height: 75px;
-			width: 75px;
-		}
-
-		.tooling {
-			display: flex;
-			flex-flow: row wrap;
-			justify-content: space-between;
-
-			&.right-side {
-				flex: 0 0 100%;
-				padding-top: 10px;
-				@media (min-width: 769px) {
-					flex: 0 0 65%;
-					padding: 0 0 0 3%;
-				}
-			}
-
-			.tool {
-				flex: 0 0 50%;
-				height: 165px;
-				text-align: center;
-				padding: 20px;
-
-				@media (min-width: 600px) {
-					flex: 0 0 20%;
-				}
-
-				&:hover {
-					background: var(--theme-color);
-					cursor: pointer;
-
-					.separator {
-						border-color: #fff;
-						transform: translateY(36px);
-					}
-
-					.stars {
-						opacity: 1;
-					}
-
-					.tool-name {
-						color: #fff;
-						transform: translateY(-35px);
-					}
-				}
-
-				svg {
-					width: 75px;
-				}
-
-				.separator {
-					display: block;
-					border-bottom: 2px solid var(--theme-color);
-					margin: 10px auto;
-					transition: transform 0.25s ease-out;
-					width: 30px;
-				}
-
-				.stars {
-					color: #fff;
-					display: inline-block;
-					font-size: 13px;
-					letter-spacing: 3px;
-					opacity: 0;
-					text-indent: 3px;
-					transition: opacity 0.25s ease-out 0.25s;
-				}
-
-				.tool-name {
-					color: var(--theme-color);
-					font-size: var(--font-small);
-					transform: translateY(-20px);
-					transition: transform 0.25s ease-out;
-				}
-			}
-		}
-	}
-
-	.tool:hover :global(path.white) {
-		fill: var(--theme-color);
-	}
-
-	.dark.is-tooling .tooling .tool :global(path.white) {
-		fill: #000;
-	}
-
-	.dark.is-tooling .tooling .tool:hover :global(path.white) {
-		fill: var(--theme-color);
-	}
-</style>
+<style src="./styles.css"></style>
